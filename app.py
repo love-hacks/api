0a4270c74013846147f6e2f6a4bf9850eefb449c
Daniela <dani.m@hotmail.co.uk>
2017-05-13 19:08:13 -0300

    Adicionar novos jogadores

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import requests
 from flask import Flask, jsonify
 
@@ -9,7 +10,8 @@ athlete =  {
 "name" : None,
 "weight" : 0,
 "photo" : None,
-"height" : 0
+"height" : 0,
+"kmPerHour" : 0
 }
 
 @app.route("/")
@@ -28,15 +30,39 @@ def athleteIndex():
 
 @app.route("/api/v1/athlete/<id>", methods=['GET'])
 def getAthlete(id):
-    response = [
-        {"status": 200, "code": 200, "description": id}
-    ]
-    response_api = requests.get(url + id, headers=headers).json()
-    result_api = response_api["resultados"]
-    athlete["photo"] = result_api["fotos"]["300x300"]
-    athlete["name"] = result_api["nome"]
-    athlete["weight"] = result_api["peso"]
-    athlete["height"] = result_api["altura"]
+    #Neymar
+    if id == "63007":
+        print "entrou"
+        response_api = requests.get(url + id, headers=headers).json()
+        result_api = response_api["resultados"]
+        athlete["photo"] = result_api["fotos"]["300x300"]
+        athlete["name"] = result_api["nome"]
+        athlete["weight"] = result_api["peso"]
+        athlete["height"] = result_api["altura"]
+        athlete["kmPerHour"] = 34.83
+        print "saiu"
+    #Messi
+    elif id == "1":
+        athlete["photo"] = "http://www.alagoas24horas.com.br/wp-content/uploads/2017/01/Messi.jpg"
+        athlete["name"] = "Lionel Andrés Messi"
+        athlete["weight"] = 72
+        athlete["height"] = 1.7
+        athlete["kmPerHour"] = 32.27
+    elif id == "2":
+        athlete["photo"] = "http://www.alagoas24horas.com.br/wp-content/uploads/2017/01/Messi.jpg"
+        athlete["name"] = "Andrés Iniesta Luján"
+        athlete["weight"] = 68
+        athlete["height"] = 1.7
+        athlete["kmPerHour"] = 31.62
+    elif id == "3":
+        #33,52
+        athlete["photo"] = "http://www.alagoas24horas.com.br/wp-content/uploads/2017/01/Messi.jpg"
+        athlete["name"] = "Gerard Piqué Bernabéu"
+        athlete["weight"] = 85
+        athlete["height"] = 1.9
+        athlete["kmPerHour"] = 33.52
+    else:
+        return jsonify([])
     print athlete
     return jsonify(athlete)
 
